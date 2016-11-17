55bf9f01b625dbae6ea02c689f05bb4e2919bab8
JesusFreak42 <TimothyMeigs@gmail.com>
2016-11-17 12:57:27 -0600

    Added (untested) code for encode functions

--- MorseTree.cpp
+++ MorseTree.cpp
@@ -12,3 +12,39 @@ void MTree::insertNode(mNode node, mNode root) {
 
 }
 void MTree::findNode(mNode node) {}
+
+string MTree::encode(string message) { //wrapper
+	stack<char> charCode;
+	string fullCode = "";
+
+	for (int i = 0; i < message.size(); i++) {
+		if (message[i] == ' ') {
+			fullCode += " ";
+		}
+		else {
+			encode(message[i], root, charCode);
+			while (!charCode.empty()) {
+				fullCode += charCode.top();
+				charCode.pop();
+			}
+		}
+	}
+}
+
+bool MTree::encode(char target, mNode *root, stack<char> & code) {
+	if (root == NULL) return false;
+
+	if (root->key == target) return true;
+
+	if (encode(target, root->mLeft, code) == true) {
+		code.push('.');
+		return true;
+	}
+	else if (encode(target, root->mRight, code) == true) {
+		code.push('-');
+		return true;
+	}
+
+	return false;
+}
+
