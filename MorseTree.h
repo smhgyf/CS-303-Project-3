6cec64447a0e7db24152392b3d6230f75762855f
JesusFreak42 <TimothyMeigs@gmail.com>
2016-12-06 19:05:19 -0600

    Added the decode functions and fixed the name of "M_Data" in them

--- MorseTree.h
+++ MorseTree.h
@@ -19,4 +19,7 @@ public:
 	void build_tree(vector<M_Data> symbols);
 	string encode(string message); //wrapper
 	bool encode(char target, M_Data *root, stack<char> & code);
+	string decode_wrapper(M_Data *Root, string code); //wrapper
+	decode(M_Data *Root, string Code);
+	string decode_letter(M_Data *Root, string code, int index);
 };
